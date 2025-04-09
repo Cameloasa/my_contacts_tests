74f28971a7f4b0f0353d9c01f77c5f2f5d667948
Camelia Ciuca <camelia1414@gmail.com>
2025-04-09 18:22:47 +0200

    friends_page, class constructor and methods

--- pages/friends_page.py
+++ pages/friends_page.py
@@ -1,30 +1,38 @@
-from dbm.sqlite3 import error
 
 from playwright.sync_api import Page
 
-def click_new_friend(page: Page):
-    page.get_by_text("Ny Vän").click()
-    page.wait_for_selector("section.form", timeout=10000)
+class FriendsPage:
 
-def fill_name(page: Page, name: str):
-    form = page.locator("section.form")
-    form.locator("input").nth(0).fill(name)
-
-def fill_email(page: Page, email: str):
-    form = page.locator("section.form")
-    form.locator("input").nth(1).fill(email)
+    def __init__(self, page: Page):
+        self.page = page
 
 
-def click_save(page: Page):
-    save_button = page.get_by_text("Spara")
-    if save_button.is_disabled():
-        error_message = page.locator("p.error").inner_text()
-        raise ValueError(f"Cannot save – error: {error_message}")
-    save_button.click()
+    def click_new_friend(self):
+        self.page.get_by_text("Ny Vän").click()
+        self.page.wait_for_selector("section.form", timeout=10000)
 
-def go_to_friend_list(page: Page):
-    page.get_by_text("Vänlista").click()
-    page.wait_for_url("https://forverkliga.se/JavaScript/my-contacts/#/friends", timeout=10000)
+    def fill_name(self, name: str):
+        form = self.page.locator("section.form")
+        form.locator("input").nth(0).fill(name)
+
+    def fill_email(self, email: str):
+        form = self.page.locator("section.form")
+        form.locator("input").nth(1).fill(email)
+
+
+    def click_save(self):
+        save_button = self.page.get_by_text("Spara")
+        if save_button.is_disabled():
+            error_message = self.page.locator("p.error").inner_text()
+            raise ValueError(f"Cannot save – error: {error_message}")
+        save_button.click()
+
+    def go_to_friend_list(self):
+        self.page.get_by_text("Vänlista").click()
+        self.page.wait_for_url("https://forverkliga.se/JavaScript/my-contacts/#/friends", timeout=10000)
+
+    def is_friend_visible(self, name: str):
+        pass
 
 
 
