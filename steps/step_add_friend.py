6b68f73b93dd431ea11a5b39ec2f3c629841f398
Camelia Ciuca <camelia1414@gmail.com>
2025-04-09 18:49:41 +0200

    friends_page, class constructor and methods, all tests passed

--- steps/step_add_friend.py
+++ steps/step_add_friend.py
@@ -1,24 +1,24 @@
 from behave import given, when, then
-from pages.friends_page import click_new_friend, fill_name, fill_email, click_save, go_to_friend_list
-
+from pages.friends_page import FriendsPage
 
 @given(u'I am on the Mina Vänner main page')
 def step_given_main_page(context):
     context.page.goto(context.base_urls["main"])
+    context.friends_page = FriendsPage(context.page)
     assert "my-contacts" in context.page.url, "Not on Mina Vänner main page"
 
 @when(u'I click "Ny Vän" and add "{name}" with email "{email}"')
 def step_when_add_friend(context,name,email):
     # Click on "Ny Vän" să open the form
-    click_new_friend(context.page)
+    context.friends_page.click_new_friend()
     # Fill input Namn and E-post
-    fill_name(context.page, name) # input: Namn
-    fill_email(context.page, email) #input: E-post
+    context.friends_page.fill_name(name) # input: Namn
+    context.friends_page.fill_email(email) #input: E-post
 
 @when(u'I try to save the friend')
 def step_when_try_to_save(context):
     try:
-        click_save(context.page)
+        context.friends_page.click_save()
     except ValueError as e:
         context.error_message = str(e)
 
@@ -33,7 +33,7 @@ def step_then_see_error_message(context):
 @when(u'I go to "Vänlista"')
 def step_when_go_to_friend_list(context):
     #Click button "Vänlista"
-    go_to_friend_list(context.page)
+    context.friends_page.go_to_friend_list()
 
 @then(u'I should see "{name}" in the friends list')
 def step_then_see_friend_in_list(context,name):
